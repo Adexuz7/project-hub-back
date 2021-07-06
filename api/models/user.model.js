8b58994730e9f2352d74a5dbd7a51ec2d59c4c5e
korodum <4bkarim.amaioua@gmail.com>
2021-07-06 11:46:38 +0100

    prove

---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+  email:{
+    type: String,
+    required: true,
+    unique: true
+  }
+
+  password:{
+    type: String,
+    required:true
+  }
+  image:{
+    type: String,
+  }
+  ideas:{
+   type: Array,
+  }
+  followers:{
+    type: Array,
+  }
+  follows:{
+    type: Array
+  }
+
+})
