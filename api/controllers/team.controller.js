a1d8869ed15c6d49feb9c83047ecc775fbce6d23
karim <4bkarim.amaioua@gmail.com>
2021-07-20 14:38:02 +0100

    user that is logged can add him/herself to a team

--- api/controllers/team.controller.js
+++ api/controllers/team.controller.js
@@ -11,18 +11,23 @@ exports.createTeam = (req, res) =>  {
 
 exports.addMemberToTeam = (req, res) => {
   TeamModel
-    .findOneAndUpdate(req.params.teamId, {$push:{ members: res.locals.user.id },new:true})
-    .then(team =>
-      res.status(200).send('User added correctly'))
+    .findOneAndUpdate(req.params.teamId)
+    .then(team => {
+      if (team.members.includes(res.locals.user.id)){
+        return res.status(200).json('User alreay in team')
+      }else if(team.members.includes(res.locals.user.id) === false){
+        team.members.push(res.locals.user.id)
+        team.save()
+        return res.status(200).json('User added')
+      }
+    })
     .catch(err => res.status(500).json(err))   
 }
 
 exports.leaveTeam = (req,res) => {
   TeamModel
     .findOneAndUpdate(res.locals.user.id, {$pull:{members: res.locals.user.id}, new:true})
-    .then(team =>
-      
-       res.status(200).send('You leave the team'))
+    .then(team => res.status(200).send('You leave the team'))
     .catch(err => res.status(500).json(err))
 }
 
