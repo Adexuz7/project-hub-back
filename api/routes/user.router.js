1cc20c4a025b653578c10af37dcab21da2299a5d
Karim Amaioua <4bkarim.amaioua@gmail.com>
2021-07-28 23:17:39 +0100

    list ready for revision

--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -2,13 +2,15 @@ const userRouter = require('express').Router()
 // const { checkAuth } = require('../../utils')
 const {
   getAllUsers,
+  getUsersByQuery,
   getUser,
   createUser
 } = require('../controllers/user.controller')
 
 userRouter
   .get('/', getAllUsers)
+  .get('/query', getUsersByQuery)
   .get('/:userId', getUser)
-  .post('/',createUser)
+  .post('/', createUser)
 
 exports.userRouter = userRouter
